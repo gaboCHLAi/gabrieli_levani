f351d79ca78d9dcfe607c275fa0beea50ce9b1d8
vasxodi <84463222+vasxodi@users.noreply.github.com>
2021-07-17 19:35:38 +0400

    Create formuls.js

---
+++ formuls.js
@@ -0,0 +1,13 @@
+ 
+function pizika1(){
+	var g = prompt("G : ", '');
+	var m1 = prompt("m1 : ", '');
+	var m2 = prompt("m2 : ", '');
+	var r = prompt("r : ", '');
+	alert (((m1*m2)*g)/(r*r));
+}
+function pizika2(){
+	var g = prompt("G : ", '');
+	var m = prompt("m2 : ", '');
+	alert ( (m*g));
+}
