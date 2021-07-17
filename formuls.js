a6177473d77585b9e0488647cceaf85063541144
vasxodi <84463222+vasxodi@users.noreply.github.com>
2021-07-17 22:27:15 +0400

    Update formuls.js

--- formuls.js
+++ formuls.js
@@ -1,13 +1,37 @@
  
 function pizika1(){
-	var g = prompt("G : ", '');
-	var m1 = prompt("m1 : ", '');
-	var m2 = prompt("m2 : ", '');
-	var r = prompt("r : ", '');
-	alert (((m1*m2)*g)/(r*r));
+	var g = prompt("რას უდრის G  ", '');
+	var m1 = prompt(" რას უდრის m1  ", '');
+	var m2 = prompt(" რას უდრის m2  ", '');
+	var r = prompt("რას უდრის r  ", '');
+	alert (' პასუხია:' +(((m1*m2)*g)/(r*r)));
 }
 function pizika2(){
-	var g = prompt("G : ", '');
-	var m = prompt("m2 : ", '');
-	alert ( (m*g));
+	var g = prompt("რას უდრის g  ", '');
+	var m = prompt("რას უდრის m  ", '');
+	alert ('პასუხია:'+( (m*g)));
+}
+function pizika3(){
+	var g = prompt("რას უდრის G ", '');
+	var m = prompt("რას უდრის m  ", '');
+	var R = prompt("რას უდრის R  ", '')
+	alert ('პასუხია:'+( (m*g)/(R*R)));
+}
+function pizika4(){
+	var g = prompt("რას უდრის A ", '');
+	var m = prompt("რას უდრის T  ", '');
+	alert ('პასუხია:'+((g / m))); 
+}
+
+function pizika5(){
+	var g = prompt("რას უდრის K  ", '');
+	var m1 = prompt(" რას უდრის q1  ", '');
+	var m2 = prompt(" რას უდრის q2  ", '');
+	var r = prompt("რას უდრის r  ", '');
+	alert (' პასუხია:' +(((m1*m2)*g)/(r*r)));
+}
+function pizika6(){
+	var k = prompt("რას უდრის K  ", '');
+	var x = prompt(" რას უდრის x  ", '');
+	alert (' პასუხია:' +(k*(x*x))/2);
 }
