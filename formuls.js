caa67eca54f874c3cb614c7fa4df011b27cf6364
levanzukakishvili <84462858+levanzukakishvili@users.noreply.github.com>
2021-07-17 22:38:54 +0400

    levanizukakishvili

--- formuls.js
+++ formuls.js
@@ -35,3 +35,48 @@ function pizika6(){
 	var x = prompt(" რას უდრის x  ", '');
 	alert (' პასუხია:' +(k*(x*x))/2);
 }
+
+
+
+
+function samkutxedisFartobi(){
+    var sFudze = document.getElementById('sFudze').value;
+    var sSimagle = document.getElementById('sSimagle').value;
+    var samkutxedisFartobi = (sFudze*sSimagle)/2;
+    var samkutxedis_Fartobi = samkutxedisFartobi;
+        
+    document.getElementById('samkutxedisFartobisShedegi').innerHTML = 'სამკუთხედის ფართობია ' + samkutxedisFartobi + ' ერთეული.';
+}
+
+
+function trapeciisFartobi(){
+    var tShuaXazi = document.getElementById('tShuaXazi').value;
+    var tSimagle = document.getElementById('tSimagle').value;
+    
+    var trapeciis_Fartobi = tShuaXazi*tSimagle;
+
+    document.getElementById('trapeciisFartobisShedegi').innerHTML = 'ტრაპეციის ფართობია '+trapeciis_Fartobi+' ერთეული.'
+}
+
+
+var measedi = 100;
+function procentisGamotvla(){
+    var sapRicxvi = parseInt(prompt('შემოიყვანეთ სასურველი რიცხვი'));
+    var procent = parseInt(prompt('შემოიყვანეთ პროცენტის სასურველი მნიშვნელობა'));
+    var daangarisheba = (sapRicxvi*procent)/measedi;
+    document.getElementById('procenti').innerHTML = 'შემოყვანილი ინფორმაციის მიხედვით მიღებული პასუხია '+daangarisheba;
+}
+
+
+function kvadratuliFesvi(){
+    var ricxvi = parseInt(prompt('შემოიყვანეთ სასურველი რიცხვი'));
+    var fesvi = Math.sqrt(ricxvi);
+    document.getElementById('kvadratuliFesvi').innerHTML = 'შემოყვანილი ინფორმაციის მიხედვით მიღებული პასუხია '+fesvi;
+}
+
+
+function kuburiFesvi(){
+    var ricxvi1 = parseInt(prompt('შემოიყვანეთ სასურველი რიცხვი'));
+    var fesvi1 = Math.cbrt(ricxvi1);
+    document.getElementById('kuburiFesvi').innerHTML = 'შემოყვანილი ინფორმაციის მიხედვით მიღებული პასუხია '+fesvi1;
+}
